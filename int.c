229ac9f71504dcc5767c5793071ed71f49bb192c
pavithraraviselvi <31505530+pavithraraviselvi@users.noreply.github.com>
2018-01-19 20:19:01 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("Enter any integer");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Given number is even");
+}
+else
+{
+printf("Given number is odd");
+}
+return0;
+}
